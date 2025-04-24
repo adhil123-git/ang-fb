e2e5b3568704c84865d6300eb882c084bd3f7c01
adhil123-git <aadhi0191@gmail.com>
2025-04-24 21:57:39 +0530

    updated

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
-  selector: 'app-homepage',
+  selector: 'app-homepage', 
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
-  constructor(private router: Router) {}
-
+export class HomepageComponent implements OnInit {
+  username: string = '';
+  constructor(private router: Router,private authenticationService: AuthenticationService) {}
+  ngOnInit(): void {
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser') || '{}');
+    this.username = loggedInUser.username || 'User';
+  }
+  
   logout(): void {
     alert('Logged out successfully!');
     localStorage.removeItem('loggedInUser'); 
